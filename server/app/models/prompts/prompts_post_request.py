604545833b7b5c5cc388c0b617cdcc8b996f2965
pboethig2 <pboethig2@gmail.com>
2024-09-01 18:41:22 +0200

    add uuid to prompt

--- server/app/models/prompts/prompts_post_request.py
+++ server/app/models/prompts/prompts_post_request.py
@@ -1,11 +1,26 @@
-from pydantic import BaseModel, Field, ConfigDict
-
+from pydantic import BaseModel, Field, field_validator
 from typing import Optional
-
+from uuid import UUID, uuid4
 
 class PromptPostRequestModel(BaseModel):
     prompt: str = Field(..., description="The text of the prompt")
     user: int = Field(..., description="The ID of the user associated with the prompt")
     status: Optional[str] = Field(None, description="The status of the prompt, if applicable")
-    uuid:  str = Field(..., description="The uuid of the prompt")
-    model_config = ConfigDict(from_attributes=True)
+    uuid: UUID = Field(..., description="A unique identifier for the prompt")
+
+    @field_validator("uuid")
+    def validate_uuid(cls, value):
+        if value:
+            return str(value)  # Convert UUID to string
+        return value
+
+    class Config:
+        from_attributes = True
+        json_schema_extra = {
+            "example": {
+                "prompt": "Test prompt",
+                "user": 123,
+                "status": "initialized",
+                "uuid": "123e4567-e89b-12d3-a456-426614174000",  # Example UUID
+            }
+        }
